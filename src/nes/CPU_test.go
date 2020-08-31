267795dab30af9a533e9507a20a6dcf842b247a7
Raúl Ferràs <raulferras@gmail.com>
2020-08-31 20:11:09 +0200

    Test push into SP wraps

--- src/nes/CPU_test.go
+++ src/nes/CPU_test.go
@@ -14,3 +14,14 @@ func TestPushIntoStack(t *testing.T) {
 	assert.Equal(t, byte(0xff-1), cpu.registers.Sp)
 	assert.Equal(t, byte(0x20), cpu.ram.read(0x1FF))
 }
+
+func TestPushIntoStackWrapsAround(t *testing.T) {
+	cpu := CreateCPU()
+	cpu.registers.Sp = 0x00
+	cpu.pushStack(0x20)
+	cpu.pushStack(0x21)
+
+	assert.Equal(t, byte(0xff-1), cpu.registers.Sp)
+	assert.Equal(t, byte(0x20), cpu.ram.read(0x100))
+	assert.Equal(t, byte(0x21), cpu.ram.read(0x1FF))
+}
