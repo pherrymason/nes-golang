0f78f4f227f814f7b5851eb3f0e0f8c69bf48448
Raúl Ferràs <raul.ferras@gmail.com>
2021-04-12 00:14:56 +0200

    Test GamePak read and write methods

--- src/nes/component/gamePak.go
+++ src/nes/component/gamePak.go
@@ -2,6 +2,12 @@ package component
 
 import "github.com/raulferras/nes-golang/src/nes/defs"
 
+const GAMEPAK_MEMORY_SIZE = 0xBFE0
+const GAMEPAK_LOW_RANGE = 0x4020
+const GAMEPAK_HIGH_RANGE = 0xFFFF
+
+const GAMEPAK_ROM_START = 0x8000
+
 type GamePak struct {
 	header Header
 	prgROM []byte
@@ -20,10 +26,10 @@ func CreateGamePak(header Header, prgROM []byte) GamePak {
 
 func (gamepak *GamePak) read(address defs.Address) byte {
 
-	offset := defs.Address(0x8000)
-	if gamepak.header.HasTrainer() {
-		offset += 512
-	}
+	offset := defs.Address(GAMEPAK_ROM_START)
+	//if gamepak.header.HasTrainer() {
+	//	offset += 512
+	//}
 
 	// NROM has mirroring from 0xC000
 	if address >= 0xC000 {
@@ -40,10 +46,10 @@ func (gamepak *GamePak) read(address defs.Address) byte {
 }
 
 func (gamepak *GamePak) write(address defs.Address, value byte) {
-	offset := defs.Address(0x8000)
-	if gamepak.header.HasTrainer() {
-		offset += 512
-	}
+	offset := defs.Address(GAMEPAK_ROM_START)
+	//if gamepak.header.HasTrainer() {
+	//	offset += 512
+	//}
 
 	// NROM has mirroring from 0xC000
 	if address >= 0xC000 {
---
+++ src/nes/component/gamePak_test.go
@@ -0,0 +1,70 @@
+package component
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_read_gamePak_without_trainer(t *testing.T) {
+	gamePak := aSampleGamePak(false)
+
+	value := gamePak.read(GAMEPAK_ROM_START)
+	assert.Equal(t, byte(0x01), value)
+}
+
+func Test_read_gamePak_on_mirror_space(t *testing.T) {
+	gamePak := aSampleGamePak(false)
+
+	value := gamePak.read(0xC100)
+	assert.Equal(t, byte(0xFF), value)
+}
+
+/*
+func Test_read_gamePak_with_trainer(t *testing.T) {
+	gamePak := aSampleGamePak(true)
+
+	value := gamePak.read(GAMEPAK_ROM_START)
+	assert.Equal(t, byte(0x02), value)
+}*/
+
+func Test_write(t *testing.T) {
+	gamePak := aSampleGamePak(false)
+	gamePak.write(GAMEPAK_ROM_START+1, 0x02)
+
+	assert.Equal(t, byte(0x02), gamePak.prgROM[0x01])
+}
+
+func Test_write_on_mirror_space(t *testing.T) {
+	gamePak := aSampleGamePak(false)
+	gamePak.write(0xC100, 0x02)
+
+	assert.Equal(t, byte(0x02), gamePak.prgROM[0xc100-0x4000-GAMEPAK_ROM_START])
+}
+
+func aSampleGamePak(withTrainer bool) GamePak {
+	data := make([]byte, GAMEPAK_MEMORY_SIZE)
+
+	// Set data in limits
+	data[0x00] = 0x01
+	data[512] = 0x02
+
+	// NROM mirroring from 0xC000
+	data[0xC100-0x4000-GAMEPAK_ROM_START] = 0xFF
+
+	flag6 := byte(0x00)
+	if withTrainer {
+		flag6 = 0x06
+	}
+	return CreateGamePak(
+		Header{
+			255,
+			255,
+			flag6,
+			0,
+			0,
+			0,
+			0,
+		},
+		data,
+	)
+}
